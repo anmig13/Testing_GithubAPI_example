d524e7b69cc9961bfcfc46778b8a42844389b0ac
anmig13 <45542048+anmig13@users.noreply.github.com>
2020-03-23 17:04:21 +0100

    Add new example project for testing Github API

---
+++ Testing_GithubAPI/main.py
@@ -0,0 +1,25 @@
+from person import Person
+
+def main():
+    # tworzymy dwa obiekty klasy Osoba
+    Jan = Person("Jan", "Nowak", 48)
+    Adam = Person("Adam", "Mickiewicz", 220)
+
+    # wywołujemy metodę przedstaw_sie() na każdym z nich
+    Jan.przedstaw_sie()
+    Adam.przedstaw_sie()
+
+    wiek_Adama_przed = Adam.urodziny()
+    Adam.przedstaw_sie()
+    print(f"Wiek Adama sprzed urodzin: {wiek_Adama_przed}")
+
+    # odwołujemy się do pól, modyfikujemy je
+    Jan.imie = "Hugo"
+    Jan.nazwisko = "Gumis"
+    Jan.wiek = 5
+
+    Jan.przedstaw_sie()
+
+
+if __name__ == "__main__":
+    main()
---
+++ Testing_GithubAPI/person.py
@@ -0,0 +1,12 @@
+
+class Person:
+    def __init__(self, imie, nazwisko, wiek):
+        self.imie = imie
+        self.nazwisko = nazwisko
+        self.wiek = wiek
+    def przedstaw_sie(self):
+        print(f"Jestem {self.imie} {self.nazwisko}. Mam {self.wiek} lat.")
+    def urodziny(self):
+        wiek_przed = self.wiek
+        self.wiek += 1
+        return wiek_przed
